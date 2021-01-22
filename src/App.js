062fa779344fb25a3ea4676d2f2b5aefdb4f1a2a
kindeme <fkounoudji@gmail.com>
2021-01-21 23:47:26 -0600

    refs propriety

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./App.css";
 import ParentCom from "./oldComponents/ParentCom";
 import PureComp from "./oldComponents/PureComp";
+import RefsDemo from "./oldComponents/RefsDemo";
 
 function App() {
   return (
     <div className="App">
-      <ParentCom />
+      <RefsDemo />
     </div>
   );
 }
---
+++ src/OldComponents/RefsDemo.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+// use for focus in input
+class RefsDemo extends Component {
+  constructor(props) {
+    super(props);
+    this.inputref = React.createRef();
+  }
+  componentDidMount() {
+    this.inputref.current.focus();
+    console.log(this.inputref);
+  }
+
+  clickHandler = () => {
+    alert(this.inputref.current.value);
+  };
+
+  render() {
+    return (
+      <div>
+        <input type="text" ref={this.inputref} />
+        <button onClick={clickHandler}></button>
+      </div>
+    );
+  }
+}
+
+export default RefsDemo;
