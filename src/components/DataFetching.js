c56ea8cacb03ad0e3c4398139d60b8afdb085fd1
kindeme <fkounoudji@gmail.com>
2021-01-21 19:41:01 -0600

    fetching data from an api and a single data

---
+++ src/components/DataFetching.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+// fetching a single element from api
+// const url = "https://jsonplaceholder.typicode.com/posts";
+// fetching a single element from api
+
+function DataFetching() {
+  // fetching agle element from api
+  // const [posts, setPosts] = useState([]);
+  /// fetching agle element from api
+  const [post, setPost] = useState({});
+  const [id, setId] = useState(1);
+
+  useEffect(() => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then((res) => {
+        console.log(res);
+        setPost(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [id]);
+  return (
+    <div>
+      {/* Fetch a sinlge element  */}
+      <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      <br />
+      {post.title}
+      {/* <ul>
+        {posts.map((post) => {
+          return <li key={post.id}>{post.title}</li>;
+        })}
+      </ul> */}
+    </div>
+  );
+}
+
+export default DataFetching;
